d22ee4900416b8774d24e281264bb966d1a0c8d1
ITSjwags <jvwagoner@gmail.com>
2020-07-05 17:44:13 -0700

    update favicon to just mark

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         background_color: `#4E4A59`,
         theme_color: `#4E4A59`,
         display: `minimal-ui`,
-        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+        icon: `src/images/logo-mark.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
