979c8e5a73d45af2d89908b22e968c08bd9de6e5
ITSjwags <jvwagoner@gmail.com>
2020-08-11 21:20:36 -0700

    feat: style 404 page better

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -6,9 +8,43 @@ import SEO from '../components/seo'
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Content>
+      <Title>NOT FOUND</Title>
+      <Copy>You just hit a route that doesn&#39;t exist... the sadness.</Copy>
+    </Content>
   </Layout>
 )
+
+const Content = styled.div`
+  margin: 20px;
+
+  @media screen and (min-width: 767px) {
+    margin: 20px auto;
+    max-width: 960px;
+  }
+`
+
+const Title = styled.h1`
+  font-size: 30px;
+  font-weight: bold;
+  letter-spacing: 1.8px;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+
+  @media screen and (min-width: 767px) {
+    font-size: 39px;
+  }
+`
+
+const Copy = styled.p`
+  font-size: 16px;
+  font-weight: 300;
+  letter-spacing: 0.8px;
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 767px) {
+    font-size: 20px;
+  }
+`
 
 export default NotFoundPage
